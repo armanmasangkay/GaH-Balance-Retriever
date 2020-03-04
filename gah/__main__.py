58fe167b608cdfa3af1b3feacda2ff82701bf8ad
armanmasangkay <vss.arman@gmail.com>
2020-03-04 15:05:00 +0800

    adding ways to connect to wifi with selenium

--- gah/__main__.py
+++ gah/__main__.py
@@ -1,14 +1,20 @@
 import sys
 from .classmodule import MyClass
 from .funcmodule import login
+from .funcmodule import createFile
+
 def main():
     print('in main')
     args = sys.argv[1:]
     print('count of args :: {}'.format(len(args)))
     for arg in args:
         print('passed argument :: {}'.format(arg))
-    login()
-    my_object = MyClass('Thomas')
-    my_object.say_name()
+        #balance check 'gah balance'
+        if (arg=='balance'):
+            login()
+            #createFile()
+
+
+    
 if __name__ == '__main__':
     main()
--- gah/funcmodule.py
+++ gah/funcmodule.py
@@ -1,44 +1,79 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ChromeOptions, Chrome
+import time
+import json
 
 def login():
-    headers={
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'
-    }
+    usernameStr='user'
+    passwordStr='crisjohn1111'
 
-    login_data={
-        'jazoest': '2708',
-        'lsd': 'AVqVRUtK',
-        'email': 'doing.art.man',
-        'pass': 'namra370h55v!',
-        'timezone': '-480',
-        'lgndim': 'eyJ3IjoxMjgwLCJoIjo4MDAsImF3IjoxMjgwLCJhaCI6NzMwLCJjIjoyNH0=',
-        'lgnrnd': '234121_0LWL',
-        'lgnjs': '1583221283',
-        'ab_test_data': 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB',
-        'locale': 'en_US',
-        'next': 'https://www.facebook.com/',
-        'login_source': 'login_bluebar',
-        'guid': 'f1763b4afd7150c',
-        'prefill_contact_point': 'doing.art.man',
-        'prefill_source': 'browser_dropdown',
-        'prefill_type': 'password',
-        'skstamp': 'eyJoYXNoIjoiYWNjZDFhOTM5ZDBlNTJmNTg3ZDZiZmRkZWRmOWJlNDEiLCJoYXNoMiI6IjU4OGZlZGFlYTMwODRiZmU0ZWFlMDJhMmRjNWEzMDYxIiwicm91bmRzIjo1LCJzZWVkIjoiNzQwMjE3MjU1NGI4NDE5ODc0ODMwNDU1ODg3NjI2ODgiLCJzZWVkMiI6ImQ2ZDRjYmM1N2ZmNDVjOTJhOTc0MDgwYjFiZTBlMDQyIiwidGltZV90YWtlbiI6NDQyOTA0LCJzdXJmYWNlIjoibG9naW4ifQ=='
-    }
+  
+    opts=ChromeOptions()
+    opts.add_experimental_option('detach',True)
 
-    with requests.Session() as s:
-        url="https://www.facebook.com"
-        r=s.get(url,headers=headers)
-        soup=BeautifulSoup(r.content,'html.parser')
-        login_data['jazoest']=soup.find('input',attrs={'name':'jazoest'})['value']
-        login_data['lsd']=soup.find('input',attrs={'name':'lsd'})['value']
-        login_data['lgndim']=soup.find('input',attrs={'name':'lgndim'})['value']
-        login_data['lgnrnd']=soup.find('input',attrs={'name':'lgnrnd'})['value']
-        login_data['lgnjs']=soup.find('input',attrs={'name':'lgnjs'})['value']
-        login_data['ab_test_data']=soup.find('input',attrs={'name':'ab_test_data'})['value']
-        login_data['guid']=soup.find('input',attrs={'name':'guid'})['value']
-        #login_data['skstamp']=soup.find('input',attrs={'name':'lsd'})['value']
+    browser=Chrome(executable_path='chromedriver',chrome_options=opts)
+    browser.get('http://192.168.254.254/html/overview.html')
 
-        r=s.post(url,data=login_data,headers=headers)
-        print(r.content)
+    loginBtn=browser.find_element_by_id('logout_span')
+    loginBtn.click()
+
+
+
+    username=browser.find_element_by_id('username')
+    username.send_keys(usernameStr)
+    password=browser.find_element_by_id('password')
+    password.send_keys(passwordStr)
+
+    popLogin=browser.find_element_by_id('pop_login')
+    popLogin.click()
+
+    #sends a message
+    send_message(browser)
     
+def waitBeforeQuit(waitTime,browser):
+    time.sleep(waitTime)
+    browser.quit()
+
+
+def send_message(browser):
+    number='8080'
+    message='DATA BAL'
+
+    smsBtn=WebDriverWait(browser,10).until(
+        EC.element_to_be_clickable((By.ID,'sms'))
+    )
+    smsBtn.click()
+
+    msgBtn=browser.find_element_by_id('message')
+    msgBtn.click()
+
+    recep=browser.find_element_by_id('recipients_number')
+    recep.send_keys(number)
+
+    mes=browser.find_element_by_id('message_content')
+    mes.send_keys(message)
+
+    popSend=browser.find_element_by_id('pop_send')
+    popSend.click()
+
+    popOk=WebDriverWait(browser,20).until(
+        EC.visibility_of_element_located((By.ID,'pop_OK'))
+    )
+    popOk.click()
+
+    #waitBeforeQuit(5,browser)
+
+def createFile():
+    data={
+        'username':'',
+        'password':''
+    }
+    with open('account.json','w') as f:
+        json.dump(data,f)
+    
+
+
+
